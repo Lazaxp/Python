39b6728663079aca05d08d8172e2a282f21e9265
Lazaxp <lasttsutz@gmail.com>
2025-08-17 18:45:00 +0800

    Update activity2.py

--- activity2.py
+++ activity2.py
@@ -1,3 +1,3 @@
 nam = input("what is your name:")
-print("\t\t* \n\t      *   * \n\t    *   Hi  * \n\t  *   ", nam,"  * \n\t    *       * \n\t      *   * \n\t\t*")
+print("Hi,", nam, "welcome to the matrix!")
 
