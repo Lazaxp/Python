f8f2ef680a133e625da8a9dfa07f1727959ab13c
Lazaxp <lasttsutz@gmail.com>
2025-08-18 11:03:39 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,8 @@
+a = 10
+print("The value of a is:", a)
+
+a += 10
+print("The value of a is:", a)
+
+a = (a%10)//5
+print("The amount of 5s in a is",a)
---
+++ activity8.py
@@ -0,0 +1,5 @@
+a = 30
+b = 31
+
+print(a < b)
+print("a is less than b")
