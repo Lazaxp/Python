3506db2bd769d3cf98d0956f89754fd1af0fccfc
Lazaxp <lasttsutz@gmail.com>
2025-09-15 15:30:26 +0800

    Create codechallenge7.py

---
+++ codechallenge7.py
@@ -0,0 +1,9 @@
+#Get the summation of every odd input
+pre = 0
+for x in range(10):
+	a = eval(input(x,"input numbers numbers 10 times"))
+    lev = a % 2
+    if lev == 1:
+		pre+=a
+print("The summation of odd inputs are:",pre)
+    
