0c84061c509381d96d610dd19e457ac9d2884afa
Lazaxp <lasttsutz@gmail.com>
2025-09-17 15:20:36 +0800

    Create codechallenge9.py

---
+++ codechallenge9.py
@@ -0,0 +1,7 @@
+#Simulate a simple countdown
+print("Welcome to the Countdown Simulator")
+bgn = eval(input("Input a number to start the countdown:\t"))
+print("Countdown Started.")
+for x in range (1, bgn+1):
+    print(x)
+print("Lift Off!")
