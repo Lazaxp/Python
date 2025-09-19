07d80e794975c9157203348737bc484f1ccbaf66
Lazaxp <lasttsutz@gmail.com>
2025-09-19 11:25:54 +0800

    Add files via upload

---
+++ code_challenge10.py
@@ -0,0 +1,16 @@
+print("what if mag simba ka")
+   res = inpt("what is your response?\nyes\nno\n>>>\t").lower()
+while bool == 0:
+ 
+    if res == 'yes':
+        print("amazing. see you at the church")
+        bool == 1
+        break
+    elif res == 'no':
+        print("come on man, what do you mean no?")
+        continue
+    else:
+        print("answer me broskie")
+    continue
+    if bool == 1:
+        break
