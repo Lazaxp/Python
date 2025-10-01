08a1c77f0ebf78e7654234fe214beab018ce95c9
Lazaxp <lasttsutz@gmail.com>
2025-10-01 10:41:37 +0800

    Add files via upload

---
+++ code_challenge13.py
@@ -0,0 +1,35 @@
+for m in range (1, 4):
+    for n in range(11,m,-1):
+        print(" ",end=" ")
+    for o in range(1,m):
+        print("*", end=" ")
+    for p in range(1,m-1):
+        print("*",end=" ")
+    print()
+print("\t\t  *",end=" ")
+for x in range (1, 6):
+    for a in range(11,x,-1):
+        print(" ",end=" ")
+    for b in range(1,x):
+        print("*", end=" ")
+    for c in range(1,x-1):
+        print("*",end=" ")
+    print()
+for y in range (2,8):
+    for d in range(11,y,-1):
+        print(" ",end=" ")
+    for e in range(1,y):
+        print("*", end=" ")
+    for f in range(1,y-1):
+        print("*",end=" ")
+    print()
+for z in range (2,10):
+    for g in range(11,z,-1):
+        print(" ",end=" ")
+    for h in range(1,z):
+        print("*", end=" ")
+    for i in range(1,z-1):
+        print("*",end=" ")
+    print()
+for l in range (1,5):
+    print("\t\t* *")
