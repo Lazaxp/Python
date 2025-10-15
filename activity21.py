98fc91a5481ac3ccb171cf92f86eccb481fa7f80
Lazaxp <lasttsutz@gmail.com>
2025-10-15 10:54:49 +0800

    Add files via upload

--- activity21.py
+++ activity21.py
@@ -1,1 +1,12 @@
-#while loops
+#application of the while loopx = True
+x = True
+while x == True:
+    kid = input('Are we there yet?\t')
+    if kid == 'no':
+        print('aww')
+        continue
+    elif kid== 'yes':
+        print('Yay!')
+        break
+    else:
+        (':(')
