0a563a58e1942669f620376485d03422d16b1d44
Lazaxp <lasttsutz@gmail.com>
2025-10-22 17:44:15 +0800

    Refactor anime recommendation program
    Fix syntax errors and improve anime recommendation logic.

--- code_challenge15.py
+++ code_challenge15.py
@@ -1,1 +1,21 @@
-
+anilist = []
+while True:
+  show = input("Reccomend any anime for me").capitalize()
+  anilist.append(show)
+  if show == "Rent-a-girlfriend" or show == "Rent a girlfriend":
+    print("pass"
+    anilist.remove(show)
+  elif show == "Initial d":
+    print("K-kansei dorifuto??")
+  elif show == "Saiki k" or show == "The disastrous life of saiki k":
+    print("GOAT.")
+  else:
+    print("cool!)
+  if show == "Exit":
+    print("Reccomendation ended")
+    break
+print("Here were your selection")
+for x in anilist
+  print(f",{x}")
+print("Thank you!")  
+    
