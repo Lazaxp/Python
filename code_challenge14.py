9a30b3688eadd05ff8174b71ccc2fbfd110371d8
Lazaxp <lasttsutz@gmail.com>
2025-10-22 10:44:28 +0800

    Implement ODD Number Compiler with input validation
    Fix syntax errors and improve input handling.

---
+++ code_challenge14.py
@@ -0,0 +1,17 @@
+y = 0
+x = True
+name = input("Hello, What is your name?")
+print("++++++++++++++++++++++++++++++++++\nODD Number Compiler")
+while x == True
+  num = eval(input("input an ODD number")
+  odd = num+","
+  if num % 2 == 1:
+    y += num
+    print("odd number")
+  elif num == 0:
+    print("process stopped")
+    break
+  else:
+    print("Invalid Number")
+print(f"Hi {name}, the total ODD numbers summation is: {y}"]
+print("all odd numbers detected were",odd)
