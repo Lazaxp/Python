3673de188a9980f7736be40c66f571069dd106c2
Lazaxp <lasttsutz@gmail.com>
2025-10-27 10:14:04 +0800

    Refactor odd number input handling in code_challenge14

--- code_challenge14.py
+++ code_challenge14.py
@@ -1,17 +1,19 @@
 y = 0
-x = True
-name = input("Hello, What is your name?")
+x = ""
+name = input("Hello, What is your name?\t")
 print("++++++++++++++++++++++++++++++++++\nODD Number Compiler")
-while x == True
-  num = eval(input("input an ODD number")
-  odd = num+","
-  if num % 2 == 1:
-    y += num
-    print("odd number")
-  elif num == 0:
-    print("process stopped")
-    break
-  else:
-    print("Invalid Number")
-print(f"Hi {name}, the total ODD numbers summation is: {y}"]
-print("all odd numbers detected were",odd)
+while True:
+    num = eval(input("input an ODD number"))
+    x = str(num)+","
+    if num % 2 == 1:
+        y += num
+        print("odd number")
+    elif num % 2 == 0:
+        print("Even number,skipping")
+    else:
+        print("Invalid Number")
+    if num == 0:
+        print("process stopped")
+        break
+print(f"Hi {name}, the total ODD numbers summation is: {y}")
+print("all odd numbers detected were",x)
