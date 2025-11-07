a24da670901b94162dee759c3dabce2c4aba1ded
Lazaxp <lasttsutz@gmail.com>
2025-11-07 11:21:38 +0800

    Update activity22.py

--- activity22.py
+++ activity22.py
@@ -2,11 +2,11 @@ import random
 
 x = random.randint(1,10)
 while True:
-g = eval(input("Guess the Number!!(1,10):\t")
-if g == x:
-  print(f"Number: {x} You Win!!")
-  break
-else:
-  print("Try again!!")
+  g = eval(input("Guess the Number!!(1,10):\t")
+  if g == x:
+    print(f"Number: {x} You Win!!")
+    break
+  else:
+    print("Try again!!")
          
          
