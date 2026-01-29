ce67e387ad9cb724abede3b90ed22223027c69d3
Lazaxp <lasttsutz@gmail.com>
2026-01-29 12:40:27 +0800

    Update Allarey_HO1.py

--- Allarey_HO1.py
+++ Allarey_HO1.py
@@ -1,24 +1,30 @@
 #Hands-on 1 ITCS103
 import os
 numlist = []
-total = 0 
+
 print("Welcome, this Hands-On 1\n")
 word = input("Type any word:\t").capitalize()
 os.system("cls")
-print(f"Word Input: {word}\n(Please Use Double Digit numbers only)\n")
-for x in range(len(word)):
-    numbers = eval(input("Enter a Number:\t"))
-    numlist.append(numbers)
-    total+=numbers
-average = total / len(word)
+print(f"Word Input: {word}\nWord Length: {len(word)}\n(Please Use Double Digit numbers only)\n")
+def ave(word):
+    total = 0
+    for x in range(len(word)):    
+        numbers = eval(input("Enter a Number:\t"))
+        numlist.append(numbers)
+        total += numbers
+    average = total / len(word)
+    return average,total
+average, total = ave(word)
+def compare(average):
+    if average <= len(word):
+        return (f"Your word ({word}) word length is LESS than the AVERAGE of your number inputs: {average}")
+    elif average >= len(word):
+        return (f"Your word ({word}) word length is GREATER than the AVERAGE of your number inputs: {average}")
+    else:
+        return (f"Your word ({word}) word length is EQUAL than the AVERAGE of your number inputs: {average}")
 os.system("cls")
-print(f"Word Input: {word}\nWord Length: {len(word)}\nNumber Inputs: {numlist}\Total: {total}\nAverage: {average}\n")
-if average <= len(word):
-    print(f"Your word ({word}) word length is LESS than the AVERAGE of your number inputs: {average}")
-elif average >= len(word):
-    print(f"Your word ({word}) word length is GREATER than the AVERAGE of your number inputs: {average}")
-else
-    print(f"Your word ({word}) word length is EQUAL than the AVERAGE of your number inputs: {average}")
+print(f"Word Input: {word}\nWord Length: {len(word)}\nNumber Inputs: {numlist}\nTotal: {total}\nAverage: {average}\n")
+print(compare(average))
 
 
 
